ab9123b6a142a4dfe5dc4793d8f92ad39dbeabb0
Leon <leoncomolli@gmail.com>
2023-11-15 21:48:01 -0300

    Se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar />
+    <ItemListContainer greeting='Bienvenidos!' />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+        <h3>Carrito</h3>
+    </div>
+  )
+}
+
+export default CartWidget
